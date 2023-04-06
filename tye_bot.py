3c58d4c7a4b3b9f1c2c0da7d7318d1391fd742bc
Dpeta <69427753+Dpeta@users.noreply.github.com>
2023-04-06 06:59:38 +0200

    Add terrible ferrari ffmpeg bingle

--- tye_bot.py
+++ tye_bot.py
@@ -1,4 +1,8 @@
 import os
+
+# import time
+# import sys
+# import subprocess
 import logging
 import random
 
@@ -40,9 +44,40 @@ class TyeBot:
     async def message(self, message):
         if message.author == self.client.user:
             return
-        elif self.client.user.mentioned_in(message) or message.content.startswith(
-            "!tye"
-        ):
+        """
+        if message.content.startswith('!ferrari'):
+            await message.channel.typing()
+            with open('ferrari.webm', 'rb') as src:
+                read = src.read()
+            length = len(read)
+            output = bytearray()
+            evil = random.randint(20000, 200000)
+            the_spliter = message.content.split(' ')
+            if len(the_spliter) == 2:
+                evil = int(the_spliter[1])
+            for x in range(0, length):
+                if random.randint(0, evil) != 0: # 60000, 40000
+                    output.append(read[x])
+                else:
+                    output.append(random.randint(0,255))
+            dest_file = f'ferrari_{time.time()}.webm'
+            converted_file = dest_file + ".mp4"
+            with open(dest_file, 'wb') as dst:
+                dst.write(output)
+            retcode = subprocess.call(f"ffmpeg -i {dest_file} -preset fast "
+                                      f"-b:a {random.randint(35,300)}k "
+                                      f"-b:v {random.randint(800,2000)}k "
+                                      f"{converted_file}", shell=True)
+            if retcode < 0:
+                print("Child was terminated by signal", -retcode, file=sys.stderr)
+                await message.channel.send('a skill issue happened,,, ohno ,,,,')
+            else:
+                print("Child returned", retcode, file=sys.stderr)
+            file = discord.File(converted_file, filename=converted_file)
+            await message.channel.send(file=file)
+            print("woo yeah woo yeah")
+        """
+        if self.client.user.mentioned_in(message) or message.content.startswith("!tye"):
             await message.channel.typing()
 
             channel_msg_str = message.content.replace("!tye ", "").replace("!tye", "")
